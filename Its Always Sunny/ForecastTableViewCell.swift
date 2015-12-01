3247d6a2c95e0d3e3903d0f11d501a051fb4f3ec
Chance Rhodes <Chance@MacBook-Pro.arrayfire.net>
2015-11-30 21:37:21 -0500

    Init Commit Weather App

---
+++ Its Always Sunny/ForecastTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ForecastTableViewCell.swift
+//  Its Always Sunny
+//
+//  Created by Chance Rhodes on 11/30/15.
+//  Copyright © 2015 Chance Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Its Always Sunny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Its Always Sunny
+//
+//  Created by Chance Rhodes on 11/30/15.
+//  Copyright © 2015 Chance Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
